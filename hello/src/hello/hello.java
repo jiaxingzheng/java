fa6ba437d446cea9f44a4026b2b7213804134dc0
jiaxingzheng <xing623457677@gmail.com>
2014-05-12 23:29:50 +0800

    new

--- hello/src/hello/hello.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-public class hello {
-	public static void main(String args[])
-	{
-		System.out.println("hello");
-	}
-
-}
---
+++ helloworld/src/helloworld/helloworld.java
@@ -0,0 +1,8 @@
+package helloworld;
+
+public class helloworld {
+	public static void main(String args[]){
+		System.out.println("hello,world!");
+	}
+
+}
